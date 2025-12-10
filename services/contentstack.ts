6c6134ca4380c4a5f3b28b2ee6cb7d611ba27066
Iain Murphy <iainmurphy@me.com>
2025-12-10 11:37:21 +0000

    Remove stack duplication

--- services/contentstack.ts
+++ services/contentstack.ts
@@ -5,7 +5,7 @@ import { EmbeddedItem } from '@contentstack/utils/dist/types/Models/embedded-obj
 import { Sdk } from '@contentstack/personalize-edge-sdk/dist/sdk'
 import { isEditButtonsEnabled, Stack } from '@/config'
 import { deserializeVariantIds } from '@/utils'
-import { Stack } from "./stack"; // your existing Stack init
+//import { Stack } from "./stack"; // your existing Stack init
 
 export async function getDailyNewsArticles() {
   const Query = Stack.ContentType("daily_news_article").Query();
