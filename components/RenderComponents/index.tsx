3fb69816238fdd9b71019f124cfe407edb2de0ae
Iain Murphy <iainmurphy@me.com>
2025-12-10 16:35:37 +0000

    Bug fix

--- components/RenderComponents/index.tsx
+++ components/RenderComponents/index.tsx
@@ -1,11 +1,21 @@
-import { VB_EmptyBlockParentClass } from '@contentstack/live-preview-utils'
-import { CardCollection, FeaturedArticles, Teaser, Text, TextAndImage } from '@/components'
-import { Page } from '@/types'
-import { pageBlocks } from '@/types/pages'
-import { isDataInLiveEdit } from '@/utils'
-import NewsSection from '../NewsSection'
+import { VB_EmptyBlockParentClass } from '@contentstack/live-preview-utils';
+import { CardCollection, FeaturedArticles, Teaser, Text, TextAndImage } from '@/components';
+import { Page } from '@/types';
+import { pageBlocks } from '@/types/pages';
+import { isDataInLiveEdit } from '@/utils';
+import NewsSection from '../NewsSection';
 
-// Extend pageBlocks to include optional news_section block
+/* --------------------------------------------
+   Types
+--------------------------------------------- */
+export interface NewsSectionProps {
+  id?: string;
+  items: any[];
+  enabled?: boolean;
+  _metadata?: any;
+}
+
+// Extend existing pageBlocks to include optional news_section
 type PageBlocksWithNews = pageBlocks & {
   news_section?: {
     enabled?: boolean;
@@ -13,102 +23,99 @@ type PageBlocksWithNews = pageBlocks & {
   };
 };
 
-/**
- * Renders different components based on the provided page data
- * @param {Object} props - Component properties
- * @param {Array} props.components - Array of page components to render
- * @param {Object} props.featured_articles - Featured articles data
- * @param {Object} props.$ - Optional object containing data-cslp attributes
- * @param {boolean} [props.isABEnabled=false] - Flag to enable A/B testing
- * @returns {JSX.Element} Rendered components
- */
+/* --------------------------------------------
+   RenderComponents Component
+--------------------------------------------- */
+function RenderComponents({
+  components,
+  featured_articles,
+  news = [],
+  $,
+  isABEnabled = false
+}: Page.pageRenderProps & { news?: any[] }) {
 
-function RenderComponents({ components, featured_articles, news = [], $, isABEnabled = false }: Page.pageRenderProps & { news?: any[] }) {
-    console.log("RENDER COMPONENTS → news prop:", news);
-    console.log("RENDER COMPONENTS → components:", components);
+  console.log("RENDER COMPONENTS → news prop:", news);
 
-const componentMapper = (component: PageBlocksWithNews, key: number) => {
+  const componentMapper = (component: PageBlocksWithNews, key: number) => {
+    switch (true) {
+      case !!component.teaser:
+        return (
+          <Teaser
+            id={`teaser-${key}`}
+            {...component.teaser}
+            isABEnabled={isABEnabled}
+          />
+        );
 
-        switch (true) {
+      case !!component.text_and_image:
+        return (
+          <TextAndImage
+            id={`text-image-${key}`}
+            {...component.text_and_image}
+          />
+        );
 
-        case (!!component.teaser):
-            return (
+      case !!component.card_collection:
+        return (
+          <CardCollection
+            id={`card-collection-${key}`}
+            {...component.card_collection}
+          />
+        );
 
-                <Teaser
-                    id={`teaser-${key}`}
-                    {...component.teaser} isABEnabled={isABEnabled}
-                />
+      case !!component.text:
+        return (
+          <Text
+            id={`text-${key}`}
+            {...component.text}
+          />
+        );
 
-            )
+      case !!component.news_section:
+        return (
+          <NewsSection
+            id={`news-section-${key}`}
+            items={news}         // Inject news items supplied from page
+            enabled={component.news_section.enabled}
+            _metadata={component.news_section._metadata}
+          />
+        );
 
-        case (!!component.text_and_image):
-            return (
-
-                <TextAndImage
-                    id={`text-image-${key}`}
-                    {...component.text_and_image}
-                />
-
-            )
-
-        case (!!component.card_collection):
-            return (
-
-                <CardCollection
-                    id={`card-collection-${key}`}
-                    {...component.card_collection}
-                />
-
-            )
-
-        case (!!component.text):
-            return (
-
-                <Text
-                    id={`text-${key}`}
-                    {...component.text}
-                />
-
-            )
-        case (!!component.news_section):
-            return (
-                
-                <NewsSection
-                    id={`news-section-${key}`}
-                    items={news}
-                    {...component.news_section}
-                export interface NewsSectionProps {
-                    id?: string;         // ← required to fix Launch error
-                    items: any[];
-                    enabled?: boolean;
-                    _metadata?: any;
-                    }
-                />
-  );
-
-        default:
-            return null
-        }
-
+      default:
+        return null;
     }
+  };
 
-    return (
-        <div>
-            <div {...((isDataInLiveEdit() && $?.components) || {})} //Parent wrapper
-                className={components?.length ? undefined : `${VB_EmptyBlockParentClass} max-height mt-32`}
-            >
-                {components?.map((component, key: number) => <div
-                    key={`component-${key}`} id={`component-${key}`}
-                    {...(isDataInLiveEdit() && $?.[`components__${key}`])}
-                >
-                    {
-                        componentMapper(component, key)
-                    }
-                </div>)}
-            </div>
-            {featured_articles && <FeaturedArticles id='card-collection-FeaturedArticles' {...featured_articles} {...$?.featured_articles} />}
-        </div>
-    )
+  return (
+    <div>
+      <div
+        {...((isDataInLiveEdit() && $?.components) || {})}
+        className={
+          components?.length
+            ? undefined
+            : `${VB_EmptyBlockParentClass} max-height mt-32`
+        }
+      >
+        {components?.map((component, key) => (
+          <div
+            key={`component-${key}`}
+            id={`component-${key}`}
+            {...(isDataInLiveEdit() && $?.[`components__${key}`])}
+          >
+            {componentMapper(component, key)}
+          </div>
+        ))}
+      </div>
+
+      {featured_articles && (
+        <FeaturedArticles
+          id="card-collection-FeaturedArticles"
+          {...featured_articles}
+          {...$?.featured_articles}
+        />
+      )}
+    </div>
+  );
 }
 
-export { RenderComponents }
+export { RenderComponents };
